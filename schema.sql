cb49a8febe162edb1b4707a4562f9407251c0e72
MasonSchnell <91274207+MasonSchnell@users.noreply.github.com>
2023-10-10 20:28:09 -0400

    Added starter code

---
+++ schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employee_tracker;
+
+USE employee_tracker;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE employee_role (
+     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+     title VARCHAR(30) NOT NULL,
+     salary INT NOT NULL,
+     department_id INT NOT NULL,
+     FOREIGN KEY (department_id)
+        REFERENCES department (id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+     first_name VARCHAR(30) NOT NULL,
+     last_name VARCHAR(30) NOT NULL,
+     employee_role_id INT NOT NULL,
+     manager_id INT,
+     FOREIGN KEY (employee_role_id)
+        REFERENCES employee_role (id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (manager_id)
+        REFERENCES employee (id)
+        ON DELETE SET NULL
+);
