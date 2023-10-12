7da2c67dc7fd228dba9ec052aa9bc89c9a153d2d
MasonSchnell <91274207+MasonSchnell@users.noreply.github.com>
2023-10-12 17:54:12 -0400

    Added functions

--- seed.sql
+++ seed.sql
@@ -1,18 +1,18 @@
 use employee_tracker;
 
-INSERT INTO department (id, name) VALUES 
-    ("1", "Marketing"),
-    ("2", "Accounting"),
-    ("3", "IT");
+INSERT INTO department (name) VALUES 
+    ("Marketing"),
+    ("Accounting"),
+    ("IT");
 
 
-INSERT INTO employee_role (id, title, salary, department_id) VALUES 
-    ("001", "Manager", 100000, 1),
-    ("002", "Assistant Manager", 70000, 1),
-    ("003", "Managers Assistant", 45000, 1);
+INSERT INTO employee_role (title, salary, department_id) VALUES 
+    ("Manager", 100000, 1),
+    ("Assistant Manager", 70000, 1),
+    ("Managers Assistant", 45000, 1);
 
 
-INSERT INTO employee (id, first_name, last_name, employee_role_id, manager_id) VALUES 
-    ("0001", "Stacy", "Bilbury", 001, NULL),
-    ("0002", "Mark", "Fairfield", 002, 0001),
-    ("0003", "Steven", "Carmichal", 003, 0001);
+INSERT INTO employee (first_name, last_name, employee_role_id, manager_id) VALUES 
+    ("Stacy", "Bilbury", 001, NULL),
+    ("Mark", "Fairfield", 002, 0001),
+    ("Steven", "Carmichal", 003, 0001);
