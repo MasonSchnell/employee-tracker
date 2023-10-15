746fb4cabb8f6df297d32faf098979e42c8672b9
MasonSchnell <91274207+MasonSchnell@users.noreply.github.com>
2023-10-14 21:11:09 -0400

    Added scrollable lists

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# employee-tracker
+# Employee Tracker
+
+This application allows users to manage their employees, roles, and departments within their organization.
+
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Acknowledgments](#acknowledgments)
+- [License](#license)
+
+## Features
+
+- View a list of all employees, departments, and roles.
+- Add new employees, departments, and roles.
+- Update an employee's role.
+- Delete employees and departments.
+
+## Installation
+1. Clone the repositroy.
+2. Navigate to the project directory.
+3. Run the following code in the terminal.
+-     npm install
+
+## Usage
+To start the application run the following code in the termainl.
+-     node index.js
+
+## Acknowledgments
+
+-   Rutgers Full Stack Development Team
+
+## License
+
+This project is licensed under the [MIT license](https://opensource.org/licenses/MIT).
--- query.sql
+++ query.sql
@@ -1,6 +1,5 @@
 USE employee_tracker;
 
--- Displays employee when given their id.
 -- SELECT 
 -- e.first_name,
 -- e.last_name,
@@ -13,23 +12,22 @@ USE employee_tracker;
 --         ON r.department_id = d.id
 -- WHERE r.id = 2;
 
--- Displays all employees.
--- CREATE VIEW all_employee_display AS
--- SELECT 
---     e.id,
---     e.first_name,
---     e.last_name,
---     r.title AS role_title,
---     d.name AS department_title,
---     r.salary,
---     CONCAT(m.first_name, " ", m.last_name) AS manager
--- FROM employee e
---     JOIN employee_role r
---         ON e.employee_role_id = r.id
---     JOIN department d
---         ON r.department_id = d.id   
---     LEFT JOIN employee m
---         on e.manager_id = m.id 
+CREATE VIEW all_employee_display AS
+SELECT 
+    e.id,
+    e.first_name,
+    e.last_name,
+    r.title AS role_title,
+    d.name AS department_title,
+    r.salary,
+    CONCAT(m.first_name, " ", m.last_name) AS manager
+FROM employee e
+    JOIN employee_role r
+        ON e.employee_role_id = r.id
+    JOIN department d
+        ON r.department_id = d.id   
+    LEFT JOIN employee m
+        on e.manager_id = m.id 
 
 -- CREATE VIEW all_departments_display AS
 -- SELECT
@@ -37,18 +35,12 @@ USE employee_tracker;
 --     department.id
 --         FROM department
 
-CREATE VIEW all_roles_display AS 
-SELECT 
-    r.id,
-    r.title,
-    d.name,
-    r.salary
-    FROM employee_role r
-        JOIN department d
-            ON r.department_id = d.id
-
--- Display all departments.
--- SELECT
---     employee_role.title,
---     employee_role.id
---         FROM employee_role
+-- CREATE VIEW all_roles_display AS 
+-- SELECT 
+--     r.id,
+--     r.title,
+--     d.name AS department_name,
+--     r.salary
+--     FROM employee_role r
+--         JOIN department d
+--             ON r.department_id = d.id
